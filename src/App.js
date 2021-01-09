3b392eb64b1e1d7599b8357e0e8f0ecb8f277b6d
ashtechin <ashutoshbundela2@gmail.com>
2021-01-09 21:17:23 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddForm';
 
 class App extends Component {
   state = {
@@ -8,11 +9,27 @@ class App extends Component {
       {id: 2, content: 'play'}
     ]
   }
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    })
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
   render (){
     return (
-      <div className="App">
+      <div className="todo-app container">
         <h1 className="center blue-text">Todos</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
